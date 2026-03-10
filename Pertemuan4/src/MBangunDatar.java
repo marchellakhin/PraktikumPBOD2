ab5a268075c18eb4f663f12323cec76270bd4afd
Marchella Arkhina Ratunesia <marchellakhina@students.undip.ac.id>
2026-03-10 14:16:52 +0700

    Pertemuan4

--- Pertemuan4/src/MBangunDatar.java
+++ Pertemuan4/src/MBangunDatar.java
@@ -7,6 +7,7 @@
 public class MBangunDatar {
     public static void main(String[] args) {
         Persegi M = new Persegi(4, "Hitam", "Putih");
+        M.printInfo();
         System.out.println("Jumlah Sisi = " + M.getJmlSisi());
         System.out.println("Luas Persegi = " + M.getLuas());
         System.out.println("Keliling Persegi = " + M.getKeliling());
