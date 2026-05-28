e1c28dfd1c8836bd5140592644fb4fd789ad7878
Marchella Arkhina Ratunesia <marchellakhina@students.undip.ac.id>
2026-05-28 13:58:16 +0700

    Pertemuan 10A

---
+++ PBO10A_24060124120036/src/MySQLPersonDAO.java
@@ -0,0 +1,20 @@
+import java.sql .*;
+/*File : MySQLPersonDAO.java
+Deskripsi : implementasi PersonDAO untuk MySQL*/
+
+public class MySQLPersonDAO implements PersonDAO{
+    public void savePerson (Person person) throws Exception {
+        String name = person.getName () ;
+        //membuat koneksi, nama db, user, password menyesuaikan
+        Class.forName("com.mysql.cj.jdbc.Driver") ;
+        Connection con = DriverManager.getConnection (
+                        "jdbc:mysql://localhost/pbo","root","marchellakhina_");
+        //kerjakan mysql query
+        String query = "INSERT INTO person (name) VALUES ('"+name+"')";
+        System.out.println (query) ;
+        Statement s = con.createStatement ();
+        s.executeUpdate (query) ;
+        //tutup koneksi database
+        con.close();
+    }
+}
